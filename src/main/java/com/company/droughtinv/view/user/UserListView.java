ffc6a9f23fd1be7e8c48445031bf4f06e320d148
adar <angeldaviddink@gmail.com>
2026-05-15 14:02:34 -0600

    Comienzo de proyecto.

---
+++ src/main/java/com/company/droughtinv/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.company.droughtinv.view.user;
+
+import com.company.droughtinv.entity.User;
+import com.company.droughtinv.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
